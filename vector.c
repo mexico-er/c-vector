836de40a9f2be7220ad48f955447c931aff173ab
mexico-er <eddy640k@gmail.com>
2024-05-25 16:33:38 -0400

    added support for custom types

--- vector.c
+++ vector.c
@@ -1,6 +1,8 @@
 #include "vector.h"
 
-void vector_init(Vector *vec, size_t initial_capacity, size_t element_size) {
+void vector_init(vector *vec, size_t initial_capacity, size_t element_size, 
+                 void (*copy_element)(void *dest, const void *src),
+                 void (*free_element)(void *element)) {
     vec->data = malloc(initial_capacity * element_size);
     if (vec->data == NULL) {
         fprintf(stderr, "Memory allocation failed\n");
@@ -9,9 +11,11 @@ void vector_init(Vector *vec, size_t initial_capacity, size_t element_size) {
     vec->size = 0;
     vec->capacity = initial_capacity;
     vec->element_size = element_size;
+    vec->copy_element = copy_element;
+    vec->free_element = free_element;
 }
 
-void vector_push_back(Vector *vec, const void *value) {
+void vector_push_back(vector *vec, const void *value) {
     if (vec->size >= vec->capacity) {
         size_t new_capacity = (vec->capacity == 0) ? 1 : vec->capacity * 2;
         void *new_data = realloc(vec->data, new_capacity * vec->element_size);
@@ -23,19 +27,15 @@ void vector_push_back(Vector *vec, const void *value) {
         vec->capacity = new_capacity;
     }
     void *dest = (char *)vec->data + vec->size * vec->element_size;
-    if (vec->element_size == sizeof(char *)) {
-        *(char **)dest = strdup(*(char **)value);
-        if (*(char **)dest == NULL) {
-            fprintf(stderr, "Memory allocation failed\n");
-            exit(EXIT_FAILURE);
-        }
+    if (vec->copy_element) {
+        vec->copy_element(dest, value);
     } else {
         memcpy(dest, value, vec->element_size);
     }
     vec->size++;
 }
 
-void *vector_at(Vector *vec, size_t index) {
+void *vector_at(vector *vec, size_t index) {
     if (index >= vec->size) {
         fprintf(stderr, "Index out of bounds\n");
         exit(EXIT_FAILURE);
@@ -43,10 +43,10 @@ void *vector_at(Vector *vec, size_t index) {
     return (char *)vec->data + index * vec->element_size;
 }
 
-void vector_free(Vector *vec) {
-    for (size_t i = 0; i < vec->size; i++) {
-        if (vec->element_size == sizeof(char *)) {
-            free(*(char **)((char *)vec->data + i * vec->element_size));
+void vector_free(vector *vec) {
+    if (vec->free_element) {
+        for (size_t i = 0; i < vec->size; i++) {
+            vec->free_element((char *)vec->data + i * vec->element_size);
         }
     }
     free(vec->data);
