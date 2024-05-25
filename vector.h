82381e06ae3bf832c67fae73d425fcb2e9144304
mexico-er <eddy640k@gmail.com>
2024-05-25 16:33:05 -0400

    Update vector.h

--- vector.h
+++ vector.h
@@ -10,9 +10,15 @@ typedef struct {
     size_t size;
     size_t capacity;
     size_t element_size;
-} Vector;
+    void (*copy_element)(void *dest, const void *src);
+    void (*free_element)(void *element);
+} vector;
 
-#define VECTOR_INIT(vec, initial_capacity, type) vector_init(&(vec), initial_capacity, sizeof(type))
+#define VECTOR_INIT(vec, initial_capacity, type) \
+    vector_init(&(vec), initial_capacity, sizeof(type), NULL, NULL)
+
+#define VECTOR_INIT_CUSTOM(vec, initial_capacity, type, copy_func, free_func) \
+    vector_init(&(vec), initial_capacity, sizeof(type), copy_func, free_func)
 
 #define VECTOR_PUSH_BACK(vec, value) \
     do { \
@@ -20,9 +26,11 @@ typedef struct {
         vector_push_back(&(vec), &__temp); \
     } while (0)
 
-void vector_init(Vector *vec, size_t initial_capacity, size_t element_size);
-void vector_push_back(Vector *vec, const void *value);
-void *vector_at(Vector *vec, size_t index);
-void vector_free(Vector *vec);
+void vector_init(vector *vec, size_t initial_capacity, size_t element_size, 
+                 void (*copy_element)(void *dest, const void *src),
+                 void (*free_element)(void *element));
+void vector_push_back(vector *vec, const void *value);
+void *vector_at(vector *vec, size_t index);
+void vector_free(vector *vec);
 
 #endif
